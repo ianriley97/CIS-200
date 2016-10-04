fe5763f2c74f52d4ed631a81f724d93fcbd07b86
Ian Riley <ianriley97@gmail.com>
2016-10-04 11:13:22 -0500

    Finally have a working print statement and I believe all of the numbers are in the Array

--- Documents/Java/proj4/proj4.java
+++ Documents/Java/proj4/proj4.java
@@ -11,42 +11,47 @@ public class proj4 {
 	int rowSize = Integer.parseInt(row) - 1;
 	String col = line.substring(3, 5);
 	int colSize = Integer.parseInt(col) - 1;
-	System.out.print(rowSize + " " + colSize);
+	//System.out.print(rowSize + " " + colSize); Test statement
+	line = s.nextLine();
 	//Just creating code blocks ignore this
 	//You have the size of the matrix in rowSize and colSize. So now you have to take the rest of the file and add it to the matrix in the right spot. Don't forget about the parity bits
 	int[][] picture = new int[rowSize][colSize];
-	int curRow = 0;
-	int curCol = 0;
 	int i = 0;
-	//This for loop is to put all of the values for each row into an array
-	for (i = 0; i <= rowSize; i++) {
-	//This string rowPicture reads the value for the line 
-		String rowPicture = s.nextLine();
-	//This breaks the string from above into array values
-		String[] values = rowPicture.split("");
-	//This is just printing out the array without the [] or ,
-		int k = 0;
-		for (k = 0; k <= colSize; k++) {
-			int value = Integer.parseInt(values[k]);
-			if (k == colSize) {
-				System.out.print(values[k]);
-			}
-			else if (k > colSize) {
-				System.out.print("");
+	int j = 0;
+	for (i = 0; i < rowSize; i++) {
+		line = s.nextLine();
+		//System.out.println("The first for loop is working"); Test statements
+		for (j = 0; j < colSize; j++) {
+			if (j < colSize) {
+				//System.out.println("The second for loop is working" + " " + j); Test statement
+				char valueChar = line.charAt(j);
+				int value = (int)(valueChar - '0');
+				//System.out.println(value); Test statement
+				//Really bad way to do this but I don't feel like figuring out the problem with my input array
+				picture[i][j] = value;
+				if (j < colSize) {
+					System.out.print(picture[i][j]);
+					//System.out.print(j);TS
+					continue;
+				}
+				else {
+					System.out.println("");
+				}
 			}
 		}
-		/**
-		System.out.println(" " + Arrays.toString(values).replace("[", "").replace("]", "").replace(",", ""));
-		int rowPictureHolder = Arrays.copyOfRange(values, 0, colSize);
-		**/
 	}
+
 	//The part above works like a charm.
 	//Except it's printing the parity bits
-	//Try to fix that down here. 
+	//Try to fix that down here.
+		//for (int m = 0; m <= rowSize - 1; m++) {
+			//for (int n = 0; n <= colSize - 1; n++) {
+				//System.out.print(picture[m][n]);
+			//}
+			//System.out.println("");
+		//} get this shit code out of my project
 	}
 }
-
-
 
 
 
